363bb953a60e4a8e647f6e14cbc1e0f09dbe43fa
Dominic <dominicshajaa@icloud.com>
2023-05-11 01:12:22 -0700

    js section started. go ready

---
+++ js/basics/arithmetic.js
@@ -0,0 +1,64 @@
+// arithmetic.js
+
+// Returns the sum of two numbers
+function add(a, b) {
+  // Implementation goes here
+}
+
+// Returns the difference of two numbers
+function subtract(a, b) {
+  // Implementation goes here
+}
+
+// Returns the product of two numbers
+function multiply(a, b) {
+  // Implementation goes here
+}
+
+// Returns the quotient of two numbers
+function divide(a, b) {
+  // Implementation goes here
+}
+
+// Returns the remainder of a division
+function modulus(a, b) {
+  // Implementation goes here
+}
+
+// Returns the result of a raised to the power of b
+function exponent(a, b) {
+  // Implementation goes here
+}
+
+// Returns the floor division of two numbers
+function floor_division(a, b) {
+  // Implementation goes here
+}
+
+// Returns the absolute value of a number
+function absolute(a) {
+  // Implementation goes here
+}
+
+// Returns a rounded number
+function round_number(a, b) {
+  // Implementation goes here
+}
+
+// Returns the square root of a number
+function square_root(a) {
+  // Implementation goes here
+}
+
+module.exports = {
+  add,
+  subtract,
+  multiply,
+  divide,
+  modulus,
+  exponent,
+  floor_division,
+  absolute,
+  round_number,
+  square_root,
+};
---
+++ js/basics/tests/arithmetic.spec.js
@@ -0,0 +1,73 @@
+// arithmetic.test.js
+const {
+  add,
+  subtract,
+  multiply,
+  divide,
+  modulus,
+  exponent,
+  floor_division,
+  absolute,
+  round_number,
+  square_root,
+} = require("../arithmetic");
+
+test("add", () => {
+  expect(add(1, 2)).toBe(3);
+  expect(add(-1, 2)).toBe(1);
+  expect(add(0, 0)).toBe(0);
+});
+
+test("subtract", () => {
+  expect(subtract(2, 1)).toBe(1);
+  expect(subtract(-1, -2)).toBe(1);
+  expect(subtract(0, 0)).toBe(0);
+});
+
+test("multiply", () => {
+  expect(multiply(2, 3)).toBe(6);
+  expect(multiply(-1, -2)).toBe(2);
+  expect(multiply(0, 1)).toBe(0);
+});
+
+test("divide", () => {
+  expect(divide(6, 2)).toBe(3);
+  expect(divide(-4, -2)).toBe(2);
+  expect(divide(0, 1)).toBe(0);
+});
+
+test("modulus", () => {
+  expect(modulus(10, 3)).toBe(1);
+  expect(modulus(-10, 3)).toBe(-1);
+  expect(modulus(0, 1)).toBe(0);
+});
+
+test("exponent", () => {
+  expect(exponent(2, 3)).toBe(8);
+  expect(exponent(-2, 3)).toBe(-8);
+  expect(exponent(0, 1)).toBe(0);
+});
+
+test("floor_division", () => {
+  expect(floor_division(10, 3)).toBe(3);
+  expect(floor_division(-10, 3)).toBe(-4);
+  expect(floor_division(0, 1)).toBe(0);
+});
+
+test("absolute", () => {
+  expect(absolute(10)).toBe(10);
+  expect(absolute(-10)).toBe(10);
+  expect(absolute(0)).toBe(0);
+});
+
+test("round_number", () => {
+  expect(round_number(10.2, 0)).toBe(10);
+  expect(round_number(10.5, 0)).toBe(11);
+  expect(round_number(-10.5, 0)).toBe(-11);
+});
+
+test("square_root", () => {
+  expect(square_root(9)).toBe(3);
+  expect(square_root(1)).toBe(1);
+  expect(square_root(0)).toBe(0);
+});
