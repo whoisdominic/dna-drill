665b52e21350f8cc9b2c5368314def0bbf6264df
Dominic <dominicshajaa@icloud.com>
2023-05-11 01:09:00 -0700

    js section started. go ready

---
+++ go/basics/arithmetic.go
@@ -0,0 +1,51 @@
+package basics
+
+// Returns the sum of two numbers
+func Add(a, b float64) float64 {
+	return 0
+}
+
+// Returns the difference of two numbers
+func Subtract(a, b float64) float64 {
+	return 0
+}
+
+// Returns the product of two numbers
+func Multiply(a, b float64) float64 {
+	return 0
+}
+
+// Returns the quotient of two numbers
+func Divide(a, b float64) float64 {
+	return 0
+}
+
+// Returns the remainder of a division
+func Modulus(a, b float64) float64 {
+	return 0
+}
+
+// Returns the result of a raised to the power of b
+func Exponent(a, b float64) float64 {
+	return 0
+}
+
+// Returns the floor division of two numbers
+func FloorDivision(a, b float64) float64 {
+	return 0
+}
+
+// Returns the absolute value of a number
+func Absolute(a float64) float64 {
+	return 0
+}
+
+// Returns a rounded number
+func RoundNumber(a float64, precision int) float64 {
+	return 0
+}
+
+// Returns the square root of a number
+func SquareRoot(a float64) float64 {
+	return 0
+}
