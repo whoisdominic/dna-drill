ce08e85190e098d205bd33b2fd892a84de807e6f
Dominic <dominicshajaa@icloud.com>
2023-06-02 18:57:34 -0700

    tools added

--- js/tools.js
+++ js/tools.js
@@ -1,3 +1,18 @@
+class LinkNode {
+  constructor(val, next = null) {
+    this.val = val
+    this.next = next
+  }
+}
+
+class TreeNode {
+  constructor(val, left = null, right = null) {
+    this.val = val
+    this.left = left
+    this.right = right
+  }
+}
+
 function generateRandomArray(size, min, max) {
   const array = []
   for (let i = 0; i < size; i++) {
@@ -6,6 +21,27 @@ function generateRandomArray(size, min, max) {
   return array
 }
 
+function generateStepArray(size) {
+  const array = []
+  for (let i = 1; i < size + 1; i++) {
+    array.push(i)
+  }
+  return array
+}
+
+function generateLinkedList(input) {
+  let head = new LinkNode(input[0], null) // first node
+  let current = head
+  for (let i = 1; i < input.length; i++) {
+    current.next = new LinkNode(input[i], null)
+    current = current.next
+  }
+  return head
+}
+
 module.exports = {
   generateRandomArray,
+  generateLinkedList,
+  generateStepArray,
+  LinkNode,
 }
